64eb994391d8296baf5aa9921a4f27ec9ed9f555
WhiteTom <whitetom@arcor.de>
2026-01-25 21:39:09 +0100

    feat: ui improvement

--- app/src/main/java/de/whitetom/luckyconnect/ui/theme/Color.kt
+++ app/src/main/java/de/whitetom/luckyconnect/ui/theme/Color.kt
@@ -2,6 +2,14 @@ package de.whitetom.luckyconnect.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+// Lucky Connect Brand Colors - Green (luck/success) and Gold accents
+val LuckyGreen80 = Color(0xFFB8E6C1)
+val LuckyGold80 = Color(0xFFFFD966)
+val LuckyGreen40 = Color(0xFF2D7A3E)
+val LuckyGold40 = Color(0xFFF4C430)
+val SurfaceGray = Color(0xFFF5F5F5)
+
+// Deprecated - keeping for reference
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
